be5b8da5d7fe5d6787b526c91f3d23053597bfaf
Don Vitaliy <donvit197seven@gmail.com>
2022-09-25 20:16:00 +0300

    aaa

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -42,6 +42,6 @@ const inputSearchHandler = (event) => {
 export const appInit = (url) => {
   createMarkup();
   creatStyle();
-  apiUrl = url || 'http://www.omdbapi.com';
+  apiUrl = url || 'https://www.omdbapi.com';
   inputSearch.addEventListener('keyup', inputSearchHandler);
 };
